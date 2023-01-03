dca0326c803850fe728ea41e71714057b0553309
user126179663 <115039826+user126179663@users.noreply.github.com>
2023-01-03 17:00:02 +0900

    Add files via upload

--- math-random.js
+++ math-random.js
@@ -5,83 +5,14 @@ class MathRandom extends HTMLElement {
 		this.tagName = 'math-random',
 		
 		this.number = 1,
-		this.prefix = '--dice-',
-		
-		this.readyHandler = rs => window.addEventListener('DOMContentLoaded', rs, this.readyOption),
-		this.readyOption = { once: true },
-		
-		this.attributeFilter = [ 'number', 'prefix' ],
-		this.childAttributeFilter = [ 'data-dice-disabled', 'data-dice-number', 'data-dice-prefix' ],
-		this.mutationObserverAttributeFilter = [ ...this.attributeFilter, ...this.childAttributeFilter ],
-		this.mutationObserverInit = {
-			attributeFilter: this.mutationObserverAttributeFilter,
-			childList: true,
-			subtree: true
-		},
+		this.prefix = 'dice',
 		
 		this.__observedAttributes = [ 'number', 'prefix' ];
 		
 	}
-	//static get observedAttributes() {
-	//	
-	//	return this.__observedAttributes;
-	//	
-	//}
-	
-	static dice(number, prefix) {
+	static get observedAttributes() {
 		
-		const { random } = Math,
-				{ dataset, style } = this, { diceNumber = number, dicePrefix } = dataset, values = [];
-		let i,l;
-		
-		i = -1, l = diceNumber === number ? number : Number.isNaN(l = parseInt(diceNumber)) || l < 0 ? number : l,
-		dicePrefix && (prefix = '--' + dicePrefix + '-');
-		while (++i < l) style.setProperty(prefix + i, values[i] = random());
-		
-		return values;
-		
-	}
-	
-	static mutated(mrs) {
-		
-		const { tagName } = MathRandom, l = mrs.length;
-		let i,i0,l0, mr,nodes,node;
-		
-		i = -1;
-		while (++i < l) {
-			
-			switch ((mr = mrs[i]).type) {
-				
-				case 'attributes':
-				(
-					(mr.target === this && MathRandom.attributeFilter.indexOf(mr.attributeName) !== -1) ||
-					(
-						mr.target.closest(tagName) === this &&
-						MathRandom.childAttributeFilter.indexOf(mr.attributeName) !== -1
-					)
-				) && (i = l);
-				break;
-				
-				case 'childList':
-				if (l0 = (nodes = mr.addedNodes).length) {
-					i0 = -1;
-					while (
-						++i0 < l0 &&
-						(
-							!((node = nodes[i0]) instanceof Element) ||
-							'data-dice-disabled' in node.dataset ||
-							node.closest(tagName) !== this
-						)
-					);
-					i0 === l0 || (i = l);
-				}
-				break;
-				
-			}
-			
-		}
-		
-		i === l || this.dice();
+		return this.__observedAttributes;
 		
 	}
 	
@@ -89,79 +20,33 @@ class MathRandom extends HTMLElement {
 		
 		super();
 		
-		const { mutated, mutationObserverInit, readyHandler, readyOption } = MathRandom;
-		
-		this.ready = document.readyState === 'interactive' ? Promise.resolve() : new Promise(readyHandler, readyOption),
-		
-		(this.observer = new MutationObserver(this.mutated = mutated.bind(this))).observe(this, mutationObserverInit);
-		
 	}
 	async connectedCallback() {
 		
 		this.dice();
 		
 	}
-	//async attributeChangedCallback() {
-	//	
-	//	const changed = this.changed = Symbol();
-	//	
-	//	await this.ready,
-	//	
-	//	changed === this.changed && this.isConnected && this.dice();
-	//	
-	//}
-	
-	dice() {
+	async attributeChangedCallback() {
 		
-		const { dice, tagName } = MathRandom, { number, prefix, style } = this;
-		let i,l, id, values;
-		
-		for (const element of this) {
-			
-			values = dice.call(element, number, prefix);
-			
-			if (id = element.id) {
-				
-				i = -1, l = values.length, id = prefix + id + '-';
-				while (++i < l) style.setProperty(id + i, values[i]);
-				
-			}
-			
-		}
+		this.isConnected && this.dice();
 		
 	}
 	
-	*[Symbol.iterator]() {
+	dice() {
 		
-		const { tagName } = MathRandom, targets = this.querySelectorAll('*'), iterated = [];
-		let i,l,i0,l0,i1,l1, li, target, ds, selector, elements,element;
+		const { random } = Math, { number, prefix, style } = this;
+		let i;
 		
-		i = li = -1, l = targets.length;
-		while (++i < l) {
+		if (number === 1) {
 			
-			if ((target = targets[i]).closest(tagName) === this) {
-				
-				if ('diceDisabled' in (ds = target.dataset)) {
-					
-					if (selector = ds.diceDisabled) {
-						
-						i0 = -1, l0 = (elements = target.querySelectorAll(selector)).length, l1 = li + 1;
-						while (++i0 < l0) {
-							i1 = -1, element = elements[i0];
-							while (++i1 < l1 && iterated[i1] !== element);
-							i1 === l1 || (iterated.splice(i1, 1), --li, --l1);
-						}
-						
-					}
-					
-				} else iterated[++li] = target;
-				
-			}
+			style.setProperty(prefix.slice(0,-1), i = random()), style.setProperty(prefix + '0', i);
+			
+		} else {
+			
+			i = -1;
+			while (++i < number) style.setProperty(prefix + i, random());
 			
 		}
-		
-		i = -1, ++li;
-		while (++i < li) yield iterated[i];
 		
 	}
 	
@@ -179,14 +64,12 @@ class MathRandom extends HTMLElement {
 	}
 	get prefix() {
 		
-		const v = this.getAttribute('prefix');
-		
-		return v ? '--' + v + '-' : MathRandom.prefix;
+		return '--' + ((this.hasAttribute('prefix') ? this.getAttribute('prefix') : this.id) || MathRandom.prefix) + '-';
 		
 	}
 	set prefix(v) {
 		
-		return this.setAttribute('prefix', v);
+		return this[(typeof v === 'boolean' ? 'toggle' : 'set') + 'Attribute']('prefix', v);
 		
 	}
 	
--- readme.md
+++ readme.md
@@ -9,28 +9,34 @@
 	</head>
 	<body>
 		
-		<math-random><div></div></math-random>
+		<math-random></math-random>
 		
 	</body>
 </html>
 ```
-　要素に指定された属性値に基づき、内包するすべての子孫要素に個別に [Math.random()](https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/Math/random) が返す値を[カスタムプロパティ](https://developer.mozilla.org/ja/docs/Web/CSS/--*)に ``--dice-*`` （``*`` は属性 *[number](#number)* に基づく任意の数）として設定します。属性 [id](https://developer.mozilla.org/ja/docs/Web/HTML/Global_attributes/id) が指定されている子孫要素に作成されたカスタムプロパティの値は、``--dice-[id]-*`` として *[\<math-random\>](#math-random-乱数埋め込み要素)* 自身にも設定されます。
+　要素に指定された属性値に基づき、[Math.random()](https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/Math/random) が返す値を[カスタムプロパティ](https://developer.mozilla.org/ja/docs/Web/CSS/--*)に設定します。カスタムプロパティは``--dice-*`` （``*`` は属性 *[number](#number)* に基づく任意の数）として設定されますが、グローバル属性 [id](https://developer.mozilla.org/ja/docs/Web/HTML/Global_attributes/id) や属性 [prefix](#prefix) によって任意の名前を指定することもできます。
 
-　乱数を値とするカスタムプロパティは *[\<math-random\>](#math-random-乱数埋め込み要素)* が文書に接続される度に更新されます。 *[\<math-random\>](#math-random-乱数埋め込み要素)* やその子孫要素の対応する属性が変化した場合、または子孫要素が追加された場合も同様です。一方で、一度作成されたカスタムプロパティは削除されません。例えば、子孫要素のひとつが *[\<math-random\>](#math-random-乱数埋め込み要素)* から削除されても、その子孫要素や、その子孫要素の指定に基づいて設定された *[\<math-random\>](#math-random-乱数埋め込み要素)* のカスタムプロパティは任意に削除しない限り残留します。
+　カスタムプロパティは *[\<math-random\>](#math-random-乱数埋め込み要素)* が文書に接続されたり、対応する属性が変化した際に更新されます。一度設定されたカスタムプロパティは任意に個別で削除しない限り残留します。
 
 ## 属性
 ### number
-　指定した自然数(0 を含む)の数だけ乱数を設定します。既定値は ``1`` です。自然数と認識できない値を指定した場合、既定値が使われます。
+　指定した自然数(0 を含む)の数だけ [Math.random()](https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/Math/random) が返す乱数を値とする[カスタムプロパティ](https://developer.mozilla.org/ja/docs/Web/CSS/--*)を設定します。既定値は ``1`` です。自然数と認識できない値を指定した場合、既定値が使われます。
+
+　この属性の値が ``1`` である時のみ、接尾辞に番号を割り振られないカスタムプロパティが追加で設定されます。既定の状態であれば、この時設定される二つのカスタムプロパティ ``--dice`` ``--dice-0`` の値は等価です。
 ### prefix
-　カスタムプロパティ名の接頭辞を指定します。既定値は ``dice`` です。
+　[カスタムプロパティ](https://developer.mozilla.org/ja/docs/Web/CSS/--*)の接頭辞を指定します。既定値は ``dice`` です。未指定かつグローバル属性 [id](https://developer.mozilla.org/ja/docs/Web/HTML/Global_attributes/id) が設定されている時は、接頭辞として id が示す値が用いられます。空文字ないし論理属性として指定すると、id が指定されていても既定値を接頭辞に強制します。
+```html
+<!-- カスタムプロパティの命名規則例 -->
 
-## 対応する子孫要素のカスタムデータ属性
-　各[カスタムデータ属性](https://developer.mozilla.org/ja/docs/Web/HTML/Global_attributes/data-*)は、指定した子孫要素に対してのみ作用します。
-### data-dice-disable
-　論理属性で、このカスタムデータ属性が指定された要素には乱数を値とするカスタムプロパティを設定しません。
+<!-- --sample, --sample-0 が設定される -->
+<math-random id="sample"></math-random>
 
-　値に任意の[セレクター](https://developer.mozilla.org/ja/docs/Web/CSS/CSS_Selectors)を指定すると、その子孫要素が内包する要素で、セレクターに一致する要素も同様に *[\<math-random\>](#math-random-乱数埋め込み要素)* の処理の対象外とします。
-### data-dice-number
-　*[\<math-random\>](#math-random-乱数埋め込み要素)* の属性 *[number](#number)* を上書きします。
-### data-dice-prefix
-　*[\<math-random\>](#math-random-乱数埋め込み要素)* の属性 *[prefix](#prefix)* を上書きします。
+<!-- --dice, --dice-0 が設定される -->
+<math-random></math-random>
+<math-random prefix></math-random>
+<math-random id="sample-0" prefix></math-random>
+
+<!-- --my-dice, --my-dice-0 が設定される -->
+<math-random prefix="my-dice"></math-random>
+<math-random id="sample-1" prefix="my-dice"></math-random>
+```
