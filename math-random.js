6591aca5b8932e322b2485012595c97ccd840d90
user126179663 <115039826+user126179663@users.noreply.github.com>
2023-01-04 02:48:17 +0900

    Add files via upload

--- math-random.js
+++ math-random.js
@@ -4,6 +4,9 @@ class MathRandom extends HTMLElement {
 		
 		this.tagName = 'math-random',
 		
+		this.int = 0,
+		this.min = 0,
+		this.max = 1,
 		this.number = 1,
 		this.prefix = 'dice',
 		
@@ -34,22 +37,70 @@ class MathRandom extends HTMLElement {
 	
 	dice() {
 		
-		const { random } = Math, { number, prefix, style } = this;
-		let i;
+		const { ceil, floor, random } = Math, { exclusive, int, number, prefix, style } = this,
+				l = number > int ? number : int, ex = exclusive ? 0 : 1, pi = prefix + 'i-';
+		let i, v,vi,range,rangeI, { max, min } = this, mini, maxi;
 		
-		if (number === 1) {
-			
-			style.setProperty(prefix.slice(0,-1), i = random()), style.setProperty(prefix + '0', i);
-			
-		} else {
-			
-			i = -1;
-			while (++i < number) style.setProperty(prefix + i, random());
-			
-		}
+		i = -1, min > max && (max = min), int && (mini = ceil(min), maxi = floor(max)),
+		range = max - min, rangeI = maxi - mini + ex;
+		while (++i < l)	i < number && style.setProperty(prefix + i, (v = random()) * range + min),
+								i < int && style.setProperty(pi + i, floor(v * rangeI + mini)),
+								i ||
+									(
+										number && style.setProperty(prefix.slice(0,-1), style.getPropertyValue(prefix + i)),
+										int && style.setProperty(pi.slice(0,-1), style.getPropertyValue(pi + i))
+									);
 		
 	}
 	
+	get exclusive() {
+		
+		return this.hasAttribute('exclusive');
+		
+	}
+	set exclusive(v) {
+		
+		this.toggleAttribute('exclusive', !!v);
+		
+	}
+	get int() {
+		
+		let v0;
+		const v =	(v0 = this.getAttribute('int')) === null ? MathRandom.int :
+							v0 === '' ? this.hasAttribute('number') ? this.number : 1 : parseInt(v0);
+		
+		return Number.isNaN(v) || v < 0 ? MathRandom.int : v;
+		
+	}
+	set int(v) {
+		
+		this.setAttribute('int', v);
+		
+	}
+	get max() {
+		
+		const v = this.hasAttribute('max') ? +this.getAttribute('max') : MathRandom.max;
+		
+		return Number.isNaN(v) ? MathRandom.max : v;
+		
+	}
+	set max(v) {
+		
+		this.setAttribute('max', v);
+		
+	}
+	get min() {
+		
+		const v = this.hasAttribute('min') ? +this.getAttribute('min') : MathRandom.min;
+		
+		return Number.isNaN(v) ? MathRandom.min : v;
+		
+	}
+	set min(v) {
+		
+		this.setAttribute('min', v);
+		
+	}
 	get number() {
 		
 		const v = parseInt(this.getAttribute('number'));
@@ -59,7 +110,7 @@ class MathRandom extends HTMLElement {
 	}
 	set number(v) {
 		
-		return this.setAttribute('number', v);
+		this.setAttribute('number', v);
 		
 	}
 	get prefix() {
@@ -69,7 +120,7 @@ class MathRandom extends HTMLElement {
 	}
 	set prefix(v) {
 		
-		return this[(typeof v === 'boolean' ? 'toggle' : 'set') + 'Attribute']('prefix', v);
+		this[(typeof v === 'boolean' ? 'toggle' : 'set') + 'Attribute']('prefix', v);
 		
 	}
 	
